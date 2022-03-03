70a420ad5ed378cb7ab37d6ad7fb142392cab01c
itaycohen97 <itay.cohen97@gmail.com>
2022-03-03 17:50:14 +0200

    first try
    app.config["SESSION_PERMANENT"] = datetime.timedelta(hours=2)

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import mysql.connector
 from dbmethods import *
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
+from tempfile import mkdtemp, gettempdir
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
 
 from helpers import apology, login_required, lookup, usd, Precent
 
@@ -31,8 +32,8 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = datetime.timedelta(hours=2)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
